a1d5f3904bda7fb916f6ac024b06d12e5d219c33
Gleb Royako <groyako2003@gmail.com>
2026-05-07 08:59:34 +0300

    Add button import from xls into product

--- inforce_product_import/__manifest__.py
+++ inforce_product_import/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '18.0.1.0.0',
     'summary': 'Import products from an XLSX file using a wizard',
     'author': 'Inforce',
-    'depends': ['product', 'uom'],
+    'depends': ['product', 'uom', 'stock'],
     'data': [
         'security/ir.model.access.csv',
         'views/product_brand_views.xml',
